068679ed6ac05bb5f815e19db0e54316aee864c9
Wolfero <ferorock44@gmail.com>
2023-05-02 14:58:54 +0100

    ✨ Create a CounterOffers domain

---
+++ app1/src/main/java/es/logixs/domain/CounterOffers.java
@@ -0,0 +1,73 @@
+package es.logixs.domain;
+
+public class ObjectId {
+    /*
+     * TODO UUID
+     * */
+}
+
+public class CounterOffers {
+    private int id;
+    private String name;
+    private String vom;
+    private double originalPrice;
+    private double counterOfferPrice;
+    private double quantity;
+
+    public CounterOffers(int id, String name, String vom, double originalPrice, double counterOfferPrice, double quantity) {
+        this.id = id;
+        this.name = name;
+        this.vom = vom;
+        this.originalPrice = originalPrice;
+        this.counterOfferPrice = counterOfferPrice;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVom() {
+        return vom;
+    }
+
+    public void setVom(String vom) {
+        this.vom = vom;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public double getCounterOfferPrice() {
+        return counterOfferPrice;
+    }
+
+    public void setCounterOfferPrice(double counterOfferPrice) {
+        this.counterOfferPrice = counterOfferPrice;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+}
