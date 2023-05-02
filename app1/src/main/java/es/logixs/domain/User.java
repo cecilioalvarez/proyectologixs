f102df39e1a31e13d7d764f2e29416e8b4d6e2c0
unknown <cmac@arquitecturajava.com>
2023-05-02 10:45:20 +0200

    añadiendo clase usuario basica

---
+++ app1/src/main/java/es/logixs/domain/User.java
@@ -0,0 +1,45 @@
+package es.logixs.domain;
+
+public class User {
+    
+    private String objectid;
+    private String name;
+    private String lastName;
+    private String email;
+    public String getObjectid() {
+        return objectid;
+    }
+    public void setObjectid(String objectid) {
+        this.objectid = objectid;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public User(String objectid) {
+        this.objectid = objectid;
+    }
+    public User(String name, String lastName, String email) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    
+
+}
