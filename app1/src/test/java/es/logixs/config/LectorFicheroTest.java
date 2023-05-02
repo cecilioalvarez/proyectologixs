4e60e1cde7bf3bedc73870417e89873f3089cb69
unknown <cmac@arquitecturajava.com>
2023-05-02 12:22:01 +0200

    lector test

---
+++ app1/src/test/java/es/logixs/config/LectorFicheroTest.java
@@ -0,0 +1,17 @@
+package es.logixs.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+public class LectorFicheroTest {
+    @Test
+    void testLeerLineas() throws IOException {
+
+        LectorFichero lector= new LectorFichero("cargaDatos.sql");
+        int lineas= lector.leerLineas().size();
+        assertEquals(1,lineas);
+    }
+}
