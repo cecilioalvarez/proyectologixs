f628029a8950e7d062902e6d01cf77b40981e98c
Wolfero <ferorock44@gmail.com>
2023-05-02 16:42:33 +0100

    ✨Create insert CounterOffers

--- app1/src/main/java/es/logixs/repository/CounterOffersRepositoryMySQL.java
+++ app1/src/main/java/es/logixs/repository/CounterOffersRepositoryMySQL.java
@@ -12,7 +12,25 @@ public class CounterOffersRepositoryMySQL implements CounterOffersRepository {
 
     @Override
     public CounterOffers insert(CounterOffers counterOffer) {
-        return null;
+        String query = "insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values(?,?,?,?,?,?)";
+
+        try (
+            Connection connection = DataBaseHelper.getConexion("mySQL");
+            PreparedStatement statement = connection.prepareStatement(query)
+        ) {
+            statement.setInt(1, counterOffer.getId());
+            statement.setString(2, counterOffer.getName());
+            statement.setString(3, counterOffer.getVom());
+            statement.setDouble(4, counterOffer.getOriginalPrice());
+            statement.setDouble(5, counterOffer.getCounterOfferPrice());
+            statement.setDouble(6, counterOffer.getQuantity());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+
+        return counterOffer;
     }
 
     @Override
@@ -24,6 +42,7 @@ public class CounterOffersRepositoryMySQL implements CounterOffersRepository {
     public void delete(CounterOffers counterOffer) {
 
     }
+
 
     @Override
     public CounterOffers findOne(int id) {
