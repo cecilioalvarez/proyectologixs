692053473f186308485c00e8c4bcd10ed61bcd8e
Wolfero <ferorock44@gmail.com>
2023-05-04 10:55:48 +0100

    Revert "✅ added should_findAll test"
    This reverts commit 85bb9e7ecf448e235c53d682c58601ada5e89c1f.

--- app1/src/test/java/es/logixs/repository/CounterOffersRepositoryTest.java
+++ app1/src/test/java/es/logixs/repository/CounterOffersRepositoryTest.java
@@ -9,9 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class CounterOffersRepositoryTest {
     private static CounterOffersRepository counterOffersRepository;
@@ -83,12 +83,5 @@ public class CounterOffersRepositoryTest {
         CounterOffers counterOffersFound = counterOffersRepository.findOne(4);
 
         assertEquals("name4",counterOffersFound.getName());
-    }
-
-    @Test
-    void  should_findAll(){
-        List<CounterOffers> counterOffersList = counterOffersRepository.findAll();
-
-        assertTrue(counterOffersList.size()>2);
     }
 }
