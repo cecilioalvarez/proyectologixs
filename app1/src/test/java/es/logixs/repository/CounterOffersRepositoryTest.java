fcd14a5917bbd729685bf3ff0b2ea7c730bdb83a
Wolfero <ferorock44@gmail.com>
2023-05-04 10:39:50 +0100

    ♻ Refactor

--- app1/src/test/java/es/logixs/repository/CounterOffersRepositoryTest.java
+++ app1/src/test/java/es/logixs/repository/CounterOffersRepositoryTest.java
@@ -4,6 +4,8 @@ import es.logixs.config.CargadorConsultasSQL;
 import es.logixs.config.DataBaseHelper;
 import es.logixs.config.LectorFichero;
 import es.logixs.domain.CounterOffers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,16 +13,31 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CounterOffersRepositoryTest {
+    private static CounterOffersRepository counterOffersRepository;
 
-    @Test
-    void should_do_insert() throws IOException {
-        CounterOffersRepository counterOffersRepository = new CounterOffersRepositoryMySQL();
+    @BeforeAll
+    public static void init() {
+        counterOffersRepository = new CounterOffersRepositoryMySQL();
+    }
+
+    @BeforeEach
+    public void inicializar() throws IOException {
         LectorFichero lector = new LectorFichero("cargadatos_counter_offers.sql");
         DataBaseHelper dataBaseHelper = new DataBaseHelper();
         CargadorConsultasSQL cargador = new CargadorConsultasSQL(lector, dataBaseHelper);
         cargador.cargarFichero();
+    }
 
-        CounterOffers counterOffers= new CounterOffers(5, "name5", "vom5",2.0,4.0,10.0);
+    @Test
+    void should_do_insert() {
+        CounterOffers counterOffers = new CounterOffers(
+            5,
+            "name5",
+            "vom5",
+            2.0,
+            4.0,
+            10.0
+        );
 
         CounterOffers counterOffersInserted = counterOffersRepository.insert(counterOffers);
 
@@ -28,14 +45,15 @@ public class CounterOffersRepositoryTest {
     }
 
     @Test
-    void should_do_update() throws IOException {
-        CounterOffersRepository counterOffersRepository = new CounterOffersRepositoryMySQL();
-        LectorFichero lector = new LectorFichero("cargadatos_counter_offers.sql");
-        DataBaseHelper dataBaseHelper = new DataBaseHelper();
-        CargadorConsultasSQL cargador = new CargadorConsultasSQL(lector, dataBaseHelper);
-        cargador.cargarFichero();
-
-        CounterOffers counterOffers= new CounterOffers(5, "name5", "vom5",2.0,4.0,5);
+    void should_do_update() {
+        CounterOffers counterOffers = new CounterOffers(
+            5,
+            "name5",
+            "vom5",
+            2.0,
+            4.0,
+            5
+        );
 
         CounterOffers counterOffersUpdated = counterOffersRepository.update(counterOffers);
 
