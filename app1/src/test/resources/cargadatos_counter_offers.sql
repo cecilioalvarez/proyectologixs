2454bfec4a06931a2957dd5a08fc21de31f5cab8
Wolfero <ferorock44@gmail.com>
2023-05-04 10:03:51 +0100

    ✨ Create cargaDatos for counteroffers repository

---
+++ app1/src/test/resources/cargadatos_counter_offers.sql
@@ -0,0 +1,14 @@
+create table CounterOffers (
+    id int not null,
+    name varchar(255) not null,
+    vom varchar(255),
+    originalPrice double not null,
+    counterOfferPrice double not null,
+    quantity double not null,
+    primary key (id)
+);
+
+insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (1,'name1','vom1',2.0,4.0,10.0);
+insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (2,'name2','vom2',2.0,4.0,10.0);
+insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (3,'name3','vom3',2.0,4.0,10.0);
+insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (4,'name4','vom4',2.0,4.0,10.0);
