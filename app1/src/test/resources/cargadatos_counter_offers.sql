e6e1ea0ba804367a7ec533d1c6704677825393fa
Wolfero <ferorock44@gmail.com>
2023-05-04 10:22:51 +0100

    🔨 update cargadatos_counter_offers.sql

--- app1/src/test/resources/cargadatos_counter_offers.sql
+++ app1/src/test/resources/cargadatos_counter_offers.sql
@@ -1,4 +1,6 @@
-create table CounterOffers (
+drop table counter_offers;
+
+create table counter_offers (
     id int not null,
     name varchar(255) not null,
     vom varchar(255),
@@ -8,7 +10,11 @@ create table CounterOffers (
     primary key (id)
 );
 
+start transaction;
+
 insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (1,'name1','vom1',2.0,4.0,10.0);
 insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (2,'name2','vom2',2.0,4.0,10.0);
 insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (3,'name3','vom3',2.0,4.0,10.0);
 insert into CounterOffers (id,name,vom,originalPrice,counterOfferPrice,quantity) values (4,'name4','vom4',2.0,4.0,10.0);
+
+commit
