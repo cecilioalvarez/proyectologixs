d47172d4c5ce0af6df4dbd3bfc539fe02fae522c
unknown <cmac@arquitecturajava.com>
2023-05-05 09:07:21 +0200

    companies

---
+++ app1/src/main/java/es/logixs/repository/CompaniesRepository.java
@@ -0,0 +1,17 @@
+package es.logixs.repository;
+
+import java.util.List;
+
+import es.logixs.domain.Companies;
+
+public interface CompaniesRepository {
+
+    Companies insert(Companies company);
+
+    void delete(Companies company);
+
+    List<Companies> findAll();
+
+    Companies findOne(String objectid);
+
+}
--- app1/src/main/java/es/logixs/repository/mysql/CompaniesRepositoryMySQL.java
+++ app1/src/main/java/es/logixs/repository/mysql/CompaniesRepositoryMySQL.java
@@ -2,6 +2,7 @@ package es.logixs.repository.mysql;
 
 import es.logixs.config.DataBaseHelper;
 import es.logixs.domain.Companies;
+import es.logixs.repository.CompaniesRepository;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,7 +11,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CompaniesRepositoryMySQL {
+public class CompaniesRepositoryMySQL implements CompaniesRepository {
 
     private final static String sqlInsert = "insert into companies (objectid,code,licenseId,name,taxId) values (?,?,?,?,?)";
     private final static String sqlFindAll = "select * from companies";
@@ -19,6 +20,7 @@ public class CompaniesRepositoryMySQL {
 
 
 
+    @Override
     public Companies insert(Companies company) {
 
         try (Connection conexion = new  DataBaseHelper().getConexion("mySQL");
@@ -37,6 +39,7 @@ public class CompaniesRepositoryMySQL {
         return company;
     }
 
+    @Override
     public void delete(Companies company) {
 
         try (Connection conexion = new  DataBaseHelper().getConexion("mySQL");
@@ -49,6 +52,7 @@ public class CompaniesRepositoryMySQL {
         }
     }
 
+    @Override
     public List<Companies> findAll() {
 
 
@@ -70,6 +74,7 @@ public class CompaniesRepositoryMySQL {
         return lista;
     }
 
+    @Override
     public Companies findOne(String objectid) {
 
 
