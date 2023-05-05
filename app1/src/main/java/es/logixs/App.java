1754525c9906e327781fb99e966c7cfe93453342
unknown <cmac@arquitecturajava.com>
2023-05-05 12:48:34 +0200

    limpieza logs

--- app1/src/main/java/es/logixs/App.java
+++ app1/src/main/java/es/logixs/App.java
@@ -1,12 +1,7 @@
 package es.logixs;
 
-import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import es.logixs.otros.ServicioPrueba;
-import es.logixs.otros2.ServicioPrueba2;
 
 /**
  * Hello world!
@@ -15,17 +10,7 @@ public final class App {
    
     private static final Logger milogger= LogManager.getLogger(App.class);
     public static void main(String[] args) {
-        milogger.info("pasamos por main");
-        ServicioPrueba servicio= new ServicioPrueba();
        
-        List<String> lista=servicio.getLista();
-
-        ServicioPrueba2 servicio2= new ServicioPrueba2();
-       
-        List<String> lista2=servicio2.getLista();
-
-        System.out.println("Hello World!");
-        milogger.info("finalizamos  main");
        
         
       
