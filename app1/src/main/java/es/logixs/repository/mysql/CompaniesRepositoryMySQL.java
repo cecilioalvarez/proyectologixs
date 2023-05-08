05ca7e2ade62257632e476d2c8aa628ef14fc152
unknown <cmac@arquitecturajava.com>
2023-05-08 09:20:34 +0200

    revision compañias

--- app1/src/main/java/es/logixs/repository/mysql/CompaniesRepositoryMySQL.java
+++ app1/src/main/java/es/logixs/repository/mysql/CompaniesRepositoryMySQL.java
@@ -18,16 +18,16 @@ public class CompaniesRepositoryMySQL implements CompaniesRepository {
     private final static String sqlInsert = "insert into companies (objectid,code,licenseId,name,taxId) values (?,?,?,?,?)";
     private final static String sqlFindAll = "select * from companies";
     private final static String sqlDelete = "delete from companies where objectid=?";
-    private final static String sqlFindOne= "select * from companies where objectid=?";
-    private static final Logger loggingTool= LogManager.getLogger(CompaniesRepositoryMySQL.class.getName());
+    private final static String sqlFindOne = "select * from companies where objectid=?";
+    private static final Logger loggingTool = LogManager.getLogger(CompaniesRepositoryMySQL.class.getName());
 
     @Override
     public Companies insert(Companies company) {
         loggingTool.info("Companies.insert() is called");
-        loggingTool.warn("The fields must be filled {}",company);
+        loggingTool.warn("The fields must be filled {}", company);
 
-        try (Connection conexion = new  DataBaseHelper().getConexion("mySQL");
-             PreparedStatement sentencia = conexion.prepareStatement(sqlInsert);) {
+        try (Connection conexion = new DataBaseHelper().getConexion("mySQL");
+                PreparedStatement sentencia = conexion.prepareStatement(sqlInsert);) {
             sentencia.setString(1, company.getObjectid());
             sentencia.setString(2, company.getCode());
             sentencia.setString(3, company.getLicenseId());
@@ -35,7 +35,8 @@ public class CompaniesRepositoryMySQL implements CompaniesRepository {
             sentencia.setString(5, company.getTaxId());
             sentencia.executeUpdate();
 
-            loggingTool.debug("Inserted company result: {},{},{},{},{}",company.getObjectid(),company.getCode(),company.getLicenseId(),company.getName(),company.getTaxId());
+            loggingTool.debug("Inserted company result: {},{},{},{},{}", company.getObjectid(), company.getCode(),
+                    company.getLicenseId(), company.getName(), company.getTaxId());
         } catch (SQLException e) {
 
             loggingTool.error("An error has occurred :", e);
@@ -46,14 +47,13 @@ public class CompaniesRepositoryMySQL implements CompaniesRepository {
     @Override
     public void delete(Companies company) {
         loggingTool.info("Companies.delete() is called");
-        loggingTool.warn("Attempting to delete {}",company);
+        loggingTool.warn("Attempting to delete {}", company);
 
-
-        try (Connection conexion = new  DataBaseHelper().getConexion("mySQL");
-             PreparedStatement sentencia = conexion.prepareStatement(sqlDelete);) {
+        try (Connection conexion = new DataBaseHelper().getConexion("mySQL");
+                PreparedStatement sentencia = conexion.prepareStatement(sqlDelete);) {
             sentencia.setString(1, company.getObjectid());
             sentencia.executeUpdate();
-            loggingTool.debug("Object company has an state of {}:",company);
+            loggingTool.debug("Object company has an state of {}:", company);
         } catch (SQLException e) {
 
             loggingTool.error("An error has occurred :", e);
@@ -66,17 +66,18 @@ public class CompaniesRepositoryMySQL implements CompaniesRepository {
         loggingTool.info("Companies.findAll() is called");
 
         List<Companies> lista = new ArrayList<Companies>();
-        loggingTool.warn("The fields must not be empty {}",lista);
+        loggingTool.warn("The fields must not be empty {}", lista);
 
-        try (Connection conn = new  DataBaseHelper().getConexion("mySQL");
-             PreparedStatement stmt = conn.prepareStatement(sqlFindAll);
-             ResultSet rs = stmt.executeQuery()) {
+        try (Connection conn = new DataBaseHelper().getConexion("mySQL");
+                PreparedStatement stmt = conn.prepareStatement(sqlFindAll);
+                ResultSet rs = stmt.executeQuery()) {
 
             while (rs.next()) {
 
-                lista.add(new Companies(rs.getString("objectid"),rs.getString("code"),rs.getString("licenseId"),rs.getString("name"),rs.getString("taxId")));
+                lista.add(new Companies(rs.getString("objectid"), rs.getString("code"), rs.getString("licenseId"),
+                        rs.getString("name"), rs.getString("taxId")));
             }
-            loggingTool.debug("List size is : {} The elements are {}",lista.size(),lista);
+            loggingTool.debug("List size is : {} The elements are {}", lista.size(), lista);
 
         } catch (SQLException e) {
 
@@ -89,22 +90,23 @@ public class CompaniesRepositoryMySQL implements CompaniesRepository {
     public Companies findOne(String objectid) {
 
         loggingTool.info("Companies.findOne() is called");
-        loggingTool.warn("Attempting to find company with identifier {}",objectid);
+        loggingTool.warn("Attempting to find company with identifier {}", objectid);
 
         Companies company = null;
 
-        try (Connection conn = new  DataBaseHelper().getConexion("mySQL");
-             PreparedStatement stmt = conn.prepareStatement(sqlFindOne);
+        try (Connection conn = new DataBaseHelper().getConexion("mySQL");
+                PreparedStatement stmt = conn.prepareStatement(sqlFindOne);
 
         ) {
             stmt.setString(1, objectid);
             try (ResultSet rs = stmt.executeQuery()) {
-                if (rs.next())
-                    company = new Companies(rs.getString("objectid"),rs.getString("code"),rs.getString("licenseId"),rs.getString("name"),rs.getString("taxId"));
-
+                if (rs.next()) {
+                    company = new Companies(rs.getString("objectid"), rs.getString("code"), rs.getString("licenseId"),
+                            rs.getString("name"), rs.getString("taxId"));
+                    loggingTool.debug("Found company result: {},{},{},{},{}", company.getObjectid(), company.getCode(),
+                            company.getLicenseId(), company.getName(), company.getTaxId());
+                }
             }
-            loggingTool.debug("Found company result: {},{},{},{},{}",company.getObjectid(),company.getCode(),company.getLicenseId(),company.getName(),company.getTaxId());
-
 
         } catch (SQLException e) {
 
